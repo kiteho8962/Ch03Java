b2531a16d5c552605a7dba16c7a8d4b124cb0cac
bit <bit@bit-PC>
2021-01-21 12:08:59 +0900

    두번째

--- src/java_syh/Syh_01.java
+++ src/java_syh/Syh_01.java
@@ -1,6 +1,9 @@
 package java_syh;
 
 public class Syh_01 {
-  System.out.println("서연호");
-
+	public static void main(String[] args) {
+		System.out.println("서연호");
+		System.out.println("java");
+	}
+  
 }
